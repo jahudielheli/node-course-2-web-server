d25a5ca3172ae32d510330bc7796fef7e510f35d
Jahudiel Heli Contreras Rodriguez <jahudiel.contreras@ctinter.com.mx>
2017-02-17 17:18:00 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('mantenimiento.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: '¡Bienvenido a mi página! :D'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+// /bad - send back JSON with erroMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'No se puede manejar la petición.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('El servidor esta funcionando en el puerto 3000');
+});
